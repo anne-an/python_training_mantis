ec1b3b9068250a610d14f33f898dee235da5e8c8
anne_morke <anne.morke@gmail.com>
2021-07-07 12:12:57 +0300

    Delete print

--- test/test_delete_project.py
+++ test/test_delete_project.py
@@ -3,7 +3,6 @@ import random
 
 def test_delete_project(app, db):
     old_projects = db.get_project_list()
-    print(old_projects)
     if not old_projects:
         app.project.create(app.project.generate_random_name())
         old_projects = db.get_project_list()
