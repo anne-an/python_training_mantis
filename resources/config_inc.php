71be9d8d923adb45aacb3d88cf5f454b0ae03ca4
anne_morke <anne.morke@gmail.com>
2021-07-07 20:18:35 +0300

    Get emails

--- resources/config_inc.php
+++ resources/config_inc.php
@@ -8,4 +8,7 @@ $g_db_password            = '';
 $g_default_timezone       = 'Europe/Berlin';
 
 $g_crypto_master_salt     = 'AOx3eNiD3sJwlAtdUxEZDsN0wlGH4EHKmc6H2ksU+q8=';
-$g_use_captcha            = off;
+$g_sighup_use_captcha     = off;
+
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = "localhost";
