6a4ecfd9bd68dbcf4182973605d293427de1cb18
anne_morke <anne.morke@gmail.com>
2021-07-07 18:23:23 +0300

    Add james integration

--- conftest.py
+++ conftest.py
@@ -29,8 +29,8 @@ def app(request, config):
     browser = request.config.getoption("--browser")
     web_config = config["web"]
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=web_config['baseUrl'])
-    fixture.session.ensure_login(username=web_config['username'], password=web_config['password'])
+        fixture = Application(browser=browser, config=config)
+    # fixture.session.ensure_login(username=web_config['username'], password=web_config['password'])
     return fixture
 
 
